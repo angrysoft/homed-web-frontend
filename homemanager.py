0b19f2b7033ab4011d615bbb1e21ec963b46f4cd
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-01-12 23:07:54 +0100

    wto, 12 sty 2021, 23:07:54 CET

--- homemanager.py
+++ homemanager.py
@@ -7,6 +7,7 @@ from typing import Dict, Any
 from time import sleep
 import logging
 from systemd.journal import JournalHandler
+import pycouchdb
 
 logger = logging.getLogger('homemanager')
 logger.addHandler(JournalHandler())
@@ -36,7 +37,7 @@ class JConfig:
 class MainWatcher:
     def __init__(self, config: JConfig) -> None:
         self.config = config
-        self.connection = None
+        self.connection:amqp.Connection
         self.connected = False
         self.sslopts = {'certfile': self.config['rabbitmq']['certfile'],
                         'keyfile': self.config['rabbitmq']['keyfile'],
@@ -44,6 +45,11 @@ class MainWatcher:
                         'password': self.config['rabbitmq']['password'],
                         "server_hostname": self.config['rabbitmq']['host']}
         self.homes:Dict[str, HomeManager] = {}
+        
+        if 'couchdb' in self.config:
+            self.db_connection:pycouchdb.Server = pycouchdb.Server(user=self.config['couchdb']['user'], password=self.config['couchdb']['password'])
+        else:
+            self.db_connection:pycouchdb.Server = pycouchdb.Server()
     
     def connect(self):
         while not self.connected:
@@ -52,12 +58,10 @@ class MainWatcher:
                                                   userid=self.config['rabbitmq']['user'], password=self.config['rabbitmq']['user_password'],
                                                   ssl=self.config['rabbitmq']['ssl'], ssl_options=self.sslopts, login_method='AMQPLAIN')
                 channel = self.connection.channel()
-                channel.exchange_delete('homedaemon')
-                # channel.queue_delete('main_q')
-                # channel.exchange_declare('homedaemon', 'topic', auto_delete=False)
-                channel.queue_declare('main_q')
-                # channel.queue_bind('main_q', 'homedaemon', routing_key='homedaemon.main')
-                channel.basic_consume(queue='main_q', callback=self.on_message, no_ack=True)
+                channel.exchange_declare('homedaemon', 'topic', auto_delete=False)
+                channel.queue_declare('main_queue')
+                channel.queue_bind('main_queue', 'homedaemon', routing_key='homedaemon.main')
+                channel.basic_consume(queue='main_queue', callback=self.on_message, no_ack=True)
                 self.connected = True
             except ConnectionRefusedError as err:
                 self.connected = False
@@ -70,7 +74,11 @@ class MainWatcher:
             event = json.loads(msg.body)
             cmd:str = event.get('cmd', '')
             if cmd == 'connect':
-                self.homes[event.get('homeid')] = HomeManager(self.connection, event.get('homeid'))
+                homeid:str = event.get('homeid', '')
+                if homeid not in self.db_connection:
+                    self.db_connection.create(homeid)
+                
+                self.homes[homeid] = HomeManager(self.connection, homeid, self.db_connection.db(homeid))
             else:
                 print(event)
                 
@@ -90,17 +98,54 @@ class MainWatcher:
                 self.connect()
 
 class HomeManager:
-    def __init__(self, connection:amqp.Connection, homeid:str) -> None:
+    def __init__(self, connection:amqp.Connection, homeid:str, db:pychoudb.db.Databse) -> None:
         self.connection = connection
         self.channel = self.connection.channel()
         self.homeid = homeid
+        self.db:pychoudb.db.Databse = db
     
     def add_queues(self):
-        self.channel.queue_declare(f'{self.homeid}_in') #, auto_delete=False)
-        self.channel.queue_declare(f'{self.homeid}_out') #, auto_delete=False)
+        self.channel.queue_declare(self.homeid)
+        self.channel.queue_bind(self.homeid, 'homedaemon', routing_key=f'homedaemon.{self.homeid}.reports')
+        self.channel.basic_consume(queue=self.homeid, callback=self.on_event, no_ack=True)
     
     def on_event(self, msg:Any):
+        actions:Dict[str, Any] = {
+            'report': self.update_device,
+            'add_device': self.add_device,
+            'del_device': self.del_device
+            }
+        
+        try:
+            event = json.loads(msg.body)
+            cmd:str = event.get('cmd', '')
+            actions.get(cmd, self._command_not_found)(event)
+        except json.JSONDecodeError as err:
+                logger.error(f'json {err} : {msg}')
+    
+    def update_device(self, event:Dict[str, Any]) -> None:
+        sid:str = event.get('sid', '')
+        if sid in self.db:
+            self.db.update(sid, event.get('data', {}))
+    
+    def add_device(self, event:Dict[str, Any]) -> None:
+         sid:str = event.get('sid', '')
+         if sid:
+             self.db[sid] = event.get('data', {})
+    
+    def del_device(self, event:Dict[str, Any]) -> None:
+        sid:str = event.get('sid', '')
+        if sid in self.db:
+            self.db.delete(sid)
+    
+    def _command_not_found(self, data:Dict[str, Any]) -> None:
         pass
+    
+    def run(self):
+        self.add_queues()
+    
+    def __del__(self):
+        self.channel.queue_unbind(self.homeid, 'homedaemon', routing_key=f'homedaemon.{self.homeid}.reports')
 
 
 if __name__ == '__main__':
