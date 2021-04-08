a6361c42879af08d883153c98bd2841fdbd3c5d4
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-04-09 01:16:26 +0200

    pią, 9 kwi 2021, 01:16:26 CEST

--- auth.py
+++ auth.py
@@ -1,51 +1,57 @@
 
+from pycouchdb.client import Client
 from starlette.authentication import (
     AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
     AuthCredentials
 )
-from starlette.requests import HTTPConnection, Request
-from typing import Tuple, Any
+from starlette.requests import HTTPConnection
+from typing import Tuple, Any, Dict
 from google.oauth2 import id_token
 from google.auth.transport import requests as g_requests
 
 class AuthBackend(AuthenticationBackend):
     async def authenticate(self, conn: HTTPConnection) -> Tuple[Any, Any]:
         if 'userid' in conn.session:
-            return AuthCredentials(["authenticated"]), SimpleUser(conn.session.get('userid', 'jondoe'))
+            return AuthCredentials(["authenticated"]), SimpleUser(conn.session.get('name', 'jondoe'))
         else:
             return AuthCredentials(), UnauthenticatedUser()
 
 
-class GoogleSignin:
-    def __init__(self, data, session) -> None:
-        self._status = 'unsigned'
-        token:str  = data.get('idtoken')
+class GoogleSignIn:
+    def __init__(self, token:str) -> None:
+        self._user_info:Dict[str,str] = {}
+        self.token = token
+        
+    async def authenticate(self):
         try:
             # Specify the CLIENT_ID of the app that accesses the backend:
-            idinfo = id_token.verify_oauth2_token(token, g_requests.Request(),
+            self._user_info = id_token.verify_oauth2_token(self.token,
+                                                  g_requests.Request(),
                                                   '877412399754-shou706hpt8q4llqenm6p93vthr4q28o.apps.googleusercontent.com')
-            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            if self._user_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                self._user_info.clear()
                 raise ValueError('Wrong issuer.')
             
             # ID token is valid. Get the user's Google Account ID from the decoded token.
-            
-            try:
-                if idinfo['sub'] in db['residents']:
-                    session['userid'] = idinfo['sub']
-                    session['locale'] = idinfo['locale']
-                    session['picture'] = idinfo['picture']
-                    session['name'] = idinfo['name']
-                    self._status = 'ok'
-                else:
-                    self._status = 'user not registered'
-            except KeyError as err:
-                self._status = f'db error {err}'
         except ValueError:
             self._status = 'Invalid token'
     
     @property
-    def status(self):
-        return self._status
+    def user_id(self) -> str:
+        return self._user_info.get('sub', '')
     
+    @property
+    def locale(self) -> str:
+        return self._user_info.get('locale', '')
     
+    @property
+    def picture(self) -> str:
+        return self._user_info.get('picture', '')
+    
+    @property
+    def name(self) -> str:
+        return self._user_info.get('name', '')
+    
+   
+        
             
--- homedweb.py
+++ homedweb.py
@@ -29,7 +29,7 @@ from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
-from starlette.responses import JSONResponse, PlainTextResponse, Response
+from starlette.responses import JSONResponse, PlainTextResponse, RedirectResponse, Response
 from starlette.templating import Jinja2Templates
 from starlette.responses import Response
 from starlette.middleware import Middleware
@@ -38,8 +38,10 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.authentication import requires
 from pycouchdb import Client
 import uvicorn
-from auth import AuthBackend
-from typing import Dict, Any, List
+from auth import AuthBackend, GoogleSignIn
+from devices import HomeManager
+from typing import Callable, Coroutine, Dict, Any, List
+from urllib.parse import quote_plus
 
 conf_file = 'tmp/homed.json'
 if not os.path.exists(conf_file):
@@ -50,24 +52,28 @@ with open(conf_file) as jfile:
     config = json.load(jfile)
 
 db:Client  = Client(f"http://{config['db']['user']}:{config['db']['password']}@localhost")
+dm = HomeManager(db)
+
 templates = Jinja2Templates(directory='templates')
 
 
 logging.basicConfig(filename='gast.log', filemode='w', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 logging.warning('app starts')
 
+from functools import wraps
 
-# def login_required(func):
-#     @wraps(func)
-#     def decorated_function(*args, **kwargs):
-#         if 'userid' not in session:
-#             return redirect(url_for('login', next=request.url))
-#         return func(*args, **kwargs)
-#     return decorated_function
+def login_required(func:Callable[[Request], Response]): # -> Coroutine[Request, Response]:
+    @wraps(func)
+    async def decorated_function(request: Request) -> Response:
+        if request.user. is_authenticated:
+            return await func(request)
+        return RedirectResponse(url=f'/signin?next={quote_plus(request.url.path)}')
+    return decorated_function
 
 
-@requires('authenticated', redirect='signin')
+@login_required
 async def home(request: Request) -> Response:
+    print(request.user)
     homeid: str = request.path_params['homeid']
     if homeid == 'favicon.ico':
         return PlainTextResponse(homeid)
@@ -76,25 +82,31 @@ async def home(request: Request) -> Response:
 
 
 async def get_all_devices(request: Request) -> Response:
-    ret: List[Dict[str,Any]] = []
-    for item in db[request.path_params['homeid']].get_all_docs():
-        ret.append(item)
-    
-    return JSONResponse(ret)
+    return JSONResponse(await dm.get_all_devices(request.path_params['homeid']))
 
 
 async def send_command(request: Request):
-    print(await request.body())
+    print(await request.body(), request.path_params['homeid'])
     return PlainTextResponse("ok")
 
 
 async def signin(request: Request):
     if request.method == 'GET':
+        print(dir(request))
+        print(request.headers)
         return templates.TemplateResponse('signin.html', {"request": request})
-        # return PlainTextResponse("ok")
     elif request.method == 'POST':
-        gs = GoogleSignin(await request.form(), request.session)
-        return PlainTextResponse(gs.status)
+        token:bytes = await request.body()
+        gs = GoogleSignIn(token.decode())
+        await gs.authenticate()
+        if gs.user_id in db['residents']:
+            request.session['userid'] = gs.user_id
+            request.session['locale'] = gs.locale
+            request.session['picture'] = gs.picture
+            request.session['name'] = gs.name
+            print(await request.query_params)
+            return RedirectResponse(url=request.query_params.get('next', '/'))
+        return PlainTextResponse('Unknown User')
     
 
 routes: List[Any] = [
@@ -109,7 +121,7 @@ routes: List[Any] = [
 if config.get('debug', False):
     middleware = [
         Middleware(SessionMiddleware, secret_key=str(urandom(24))),
-        Middleware(AuthenticationMiddleware, backend=GoogleAuthBackend())
+        Middleware(AuthenticationMiddleware, backend=AuthBackend())
     ]
 else:
     middleware = [
