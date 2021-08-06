bf5a1ca04bdc00548bba2ad8e6af7212bac14f03
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-08-06 21:42:14 +0200

    pią, 6 sie 2021, 21:42:14 CEST

--- homedweb.py
+++ homedweb.py
@@ -121,9 +121,9 @@ async def sse(request: Request):
     return EventSourceResponse(messages(homeid))
 
 
-conf_file = '/etc/homedaemon/homemanager.json'
+conf_file = '/etc/homedaemon/homed.json'
 if not os.path.exists(conf_file):
-    raise FileNotFoundError('can`find config file /etc/homedaemon/homemanager.json')
+    raise FileNotFoundError('can`find config file /etc/homedaemon/homed.json')
 
 config: Dict[str, Any] = {}
 with open(conf_file) as jfile:
