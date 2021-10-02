d4f5121f8e9be036aa4aa472deea2449536c7efe
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-10-02 21:56:40 +0200

    sob, 2 paź 2021, 21:56:40 CEST

--- src/sw.ts
+++ src/sw.ts
@@ -7,11 +7,21 @@ var urlsCache = [
     '/static'
 ];
 
-self.addEventListener('install', event => {
+/* self.addEventListener('install', event => {
     const preCache = async () => {
       const cache = await caches.open('static-v1');
       return await cache.addAll(urlsCache);
     };
     // @ts-ignore: Unreachable code error
     event.waitUntil(preCache());
-  });
+  }); */
+
+self.addEventListener('install', ev=> {
+    // @ts-ignore: Unreachable code error
+    ev.waitUntil(
+        caches.open(HOME_CACHE_NAME)
+        .then((cache )=> {
+            return cache.addAll(urlsCache);
+        })
+    );
+});
