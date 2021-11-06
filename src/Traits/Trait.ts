6b6e315b9de6151364ccbc55d232d8756289599e
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-11-06 22:45:37 +0100

    sob, 6 lis 2021, 22:45:37 CET

---
+++ src/Traits/Trait.ts
@@ -0,0 +1,26 @@
+import { BaseComponent } from "../components.js";
+
+export class Trait extends BaseComponent {
+    protected _sendCommands: boolean = false;
+    protected statusList: Array<string> = [];
+    protected _showInMainView: boolean = false;
+
+    constructor() {
+        super();
+    }
+
+    public getStatusList(deviceInfo: Object): Array<string> {
+        return this.statusList;
+    }
+
+    public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    }
+
+    get sendCommands() {
+        return this._sendCommands;
+    }
+
+    get showInMainView(): boolean {
+        return this._showInMainView;
+    }
+}
