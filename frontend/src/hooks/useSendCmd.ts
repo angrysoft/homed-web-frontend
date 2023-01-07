06100923ea490d2540726bf8d7df9596c3d52599
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-01-07 13:54:00 +0100

    sendcmd

--- frontend/src/hooks/useSendCmd.ts
+++ frontend/src/hooks/useSendCmd.ts
@@ -2,6 +2,20 @@
 const useSendCmd = () => {
   const send = (sid:string, cmd:string, args: any) => {
     console.log(`execute.${sid}.${cmd}.${args.toString()}`, args)
+    const fetchOptions: any = {
+      method:  "POST",
+      cache: "no-cache",
+      body: `execute.${sid}.${cmd}.${args.toString()}`,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+    };
+    
+    fetch("/devices", fetchOptions).then((response) => {
+      if (!response.ok) {
+          console.error("Response error: ", response.status);
+        };
+    });
   }
 
   return send;
