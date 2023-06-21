91fcfccf1a0ac6441d04e264a7070062c4879e66
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-06-21 16:34:57 +0200

    scroll into place

--- frontend/src/pages/home/places.tsx
+++ frontend/src/pages/home/places.tsx
@@ -40,14 +40,17 @@ const Places: React.FC<IPlacesProps> = (props: IPlacesProps) => {
 
 const Place: React.FC<IPlaceProps> = (props: IPlaceProps) => {
   let classes: string = "border-b-4 py-1 px-2 transition-all delay-150 whitespace-nowrap text-center";
+  const el = document.getElementById(props.name);
   if (props.selected) {
     classes += " border-primary";
+    if (el)
+      el.scrollIntoView({ behavior: 'smooth', inline: "center" });
   } else {
     classes += " border-surface";
   }
 
   return (
-    <div className={classes} onClick={()=> props.handelClick()}>
+    <div id={props.name} className={classes} onClick={()=> props.handelClick()}>
       {props.name}
     </div>
   );
