240a9bd4a5dce6c2f3a8e3ec0d64d2e2f7cd615a
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-03-16 23:25:06 +0100

    rewrites

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-     async rewrites() {
-        return [
-          {
-            source: '/api/v1/devices/:path*',
-            destination: 'http://localhost:8080/api/v1/devices/:path*',
-          },
-        ]
-      },
-      output: "standalone"
+    async rewrites() {
+       return [
+         {
+           source: '/api/v1/:path*',
+           destination: 'http://localhost:8080/api/v1/:path*',
+         },
+         {
+           source: '/oauth2/authorization/google',
+           destination: 'http://localhost:8080//oauth2/authorization/google',
+         },
+       ]
+     },
+     output: "standalone"
 }
 
 module.exports = nextConfig
