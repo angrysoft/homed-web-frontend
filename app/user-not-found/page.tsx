8348b2e87822a4266adb811ad05724c636f44062
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-03-16 23:33:20 +0100

    user not found page

---
+++ app/user-not-found/page.tsx
@@ -0,0 +1,7 @@
+export default function UserNotFound() {
+  return (
+    <div className="grid place-content-center p-05 h-dvh">
+      <span className="p-1 border-error border rounded-lg">Sorry you can&apos;t login with this google account</span>
+    </div>
+  );
+}
